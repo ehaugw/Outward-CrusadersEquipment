910d634a2b757c861ed289132b935837f0129b87
Eivind <ehaugw@gmail.com>
2022-11-27 13:50:04 +0100

    enable for elatts sanctity

--- Items/CrusadersArmor.cs
+++ Items/CrusadersArmor.cs
@@ -5,6 +5,7 @@ namespace CrusadersEquipment
     using SideLoader;
     using InstanceIDs;
     using HolyDamageManager;
+    using TinyHelper;
 
     public class CrusadersArmor
     {
@@ -88,6 +89,8 @@ namespace CrusadersEquipment
                 RecipeUID = newUID
             };
             myitem.ApplyTemplate();
+
+            TinyItemManager.AddEnchantingOption(myitem.New_ItemID, IDs.elattsSanctityChestID);
             return ResourcesPrefabManager.Instance.GetItemPrefab(myitem.New_ItemID);
         }
     }
